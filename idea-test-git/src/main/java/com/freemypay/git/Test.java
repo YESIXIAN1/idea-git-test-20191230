78e7a67d7f6ab47fefccf584e6b18e4c67456259
yesixian <asd6195090>
2019-12-30 17:01:15 +0800

    我是MASTER,我要测试分支

--- idea-test-git/src/main/java/com/freemypay/git/Test.java
+++ idea-test-git/src/main/java/com/freemypay/git/Test.java
@@ -7,6 +7,7 @@ package com.freemypay.git;
  * TIME: 15:18
  */
 public class Test {
+    // 我是master，我要测试分支
     public static void main(String[] args) {
         System.out.println("hello world!");
     }
---
+++ idea-test-git/src/main/java/com/freemypay/git/user.java
@@ -0,0 +1,10 @@
+package com.freemypay.git;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ${ysxian}
+ * Date: 2019/12/30
+ * TIME: 17:00
+ */
+public class user {
+}
