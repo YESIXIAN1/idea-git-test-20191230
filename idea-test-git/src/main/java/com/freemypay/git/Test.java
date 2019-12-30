c7773cd3e9b74e2f2c2ff6bcb967425eec57bbca
yesixian <asd6195090>
2019-12-30 16:09:49 +0800

    idea-git-test 初次提交

---
+++ idea-test-git/src/main/java/com/freemypay/git/Test.java
@@ -0,0 +1,14 @@
+package com.freemypay.git;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ${ysxian}
+ * Date: 2019/12/30
+ * TIME: 15:18
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+
+}
